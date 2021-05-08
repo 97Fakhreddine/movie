40d907a949509647c16c3aac9cfd94493a7fb9a9
97Fakhreddine <fakhri19971998@gmail.com>
2021-05-08 06:05:29 +0100

    404, copyright components has been created

---
+++ src/config/components/NOT_FOUND.tsx
@@ -0,0 +1,70 @@
+import { makeStyles, Theme } from "@material-ui/core/styles";
+import Typography from "@material-ui/core/Typography";
+import Typed from "react-typed";
+import Button from "@material-ui/core/Button";
+import { useHistory } from "react-router";
+
+import image404 from "../../assets/images/page404animation.gif";
+import "../../assets/styles/pageNotFound.css";
+
+const useStyles = makeStyles((theme: Theme) => ({
+  avatar: {
+    width: theme.spacing(55),
+    height: theme.spacing(55),
+    margin: theme.spacing(1),
+  },
+  title: {
+    color: "tomato",
+  },
+  subtitle: {
+    color: "tan",
+    textTransform: "uppercase",
+  },
+  typedContainer: {
+    position: "absolute",
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%,-50%)",
+    width: "100vw",
+    textAlign: "center",
+    zIndex: 12,
+  },
+}));
+const NOT_FOUND = () => {
+  const classes = useStyles();
+
+  const screenSize = window.screen.width;
+
+  const history = useHistory();
+
+  const goBackHome = () => {
+    history.push("/");
+  };
+
+  return (
+    <div className='container'>
+      <div className='image-btn'>
+        <img src={image404} alt='' />
+        <Button
+          size={screenSize < 900 ? "small" : "large"}
+          color='secondary'
+          onClick={goBackHome}>
+          Go Back Home
+        </Button>
+      </div>
+      <Typography className={classes.title} variant='h1'>
+        <Typed
+          strings={[
+            "This page could not be found",
+            "It might be removed or not found",
+          ]}
+          typeSpeed={100}
+          backSpeed={100}
+          loop
+        />
+      </Typography>
+    </div>
+  );
+};
+
+export default NOT_FOUND;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,12 @@
+import { Copyright } from "../config/components/CopyRight";
+import NOT_FOUND from "../config/components/NOT_FOUND";
+
 export const Home = () => {
-  return <h1>this is home</h1>;
+  return (
+    <div>
+      <h1>this is home</h1>
+      <NOT_FOUND />
+      <Copyright />
+    </div>
+  );
 };
