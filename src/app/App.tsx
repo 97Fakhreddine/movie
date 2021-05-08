3d3fd94c9a7bc4a8fa48a6e65560046eee25b8e3
97Fakhreddine <fakhri19971998@gmail.com>
2021-05-08 06:33:34 +0100

    first version layout of the home page

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -3,6 +3,7 @@ import { ProtectedRoute } from "../config/auth/ProtectedRoutes";
 import { Home } from "../pages/Home";
 import { Login } from "../pages/Login";
 import { SignIn } from "../pages/SignIn";
+import NOT_FOUND from "../components/NOT_FOUND";
 
 function App() {
   return (
@@ -21,6 +22,7 @@ function App() {
           component={SignIn}
         />
         <ProtectedRoute authenticated={true} path='/' exact component={Home} />
+        <ProtectedRoute exact authenticated={true} component={NOT_FOUND} />
       </Switch>
     </BrowserRouter>
   );
